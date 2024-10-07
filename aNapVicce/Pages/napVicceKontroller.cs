41a174229f7bc198f5081d7b9a88f64e9b3551a0
KricsDavid <david@kricsfalusi.hu>
2024-10-07 08:55:40 +0200

    Add project files.

---
+++ aNapVicce/Pages/napVicceKontroller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aNapVicce.Pages
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class napVicceKontroller : Controller
+    {
+        public IActionResult Napvicce()
+        {
+            return Ok("Vicc");
+        }
+    }
+}
